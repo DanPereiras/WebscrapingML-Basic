c1c7954ea726d5fae6c2b08e2798353f1ea4d01f
Daniel Pereira <danpereiras@outlook.com>
2025-01-17 09:28:47 -0300

    Scrapy.py

--- Scrapy.py
+++ Scrapy.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 url = "https://lista.mercadolivre.com.br/teclado-mecanico#D[A:Teclado%20mecanico]" #LINK QUE DESEJA EXTRAIR OS DADOS
 headers = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"
+    'User-Agent': ""
 } # DEFINE O HEADER PARA O SITE RECONHECER O DISPOSITIVO
 
 response = requests.get(url, headers=headers) #RESPOSTA DA REQUISIÇÃO
